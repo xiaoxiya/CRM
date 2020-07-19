f3b65055b928f193a1c413be0e5343fb4bfd58b8
xiaoxiya <1292733831@qq.com>
2020-07-19 20:52:30 +0800

    希尔排序

--- DataStructuresAndAlgorithmAnalysis-InJava/src/chapter07/ShellSort.java
+++ DataStructuresAndAlgorithmAnalysis-InJava/src/chapter07/ShellSort.java
@@ -9,7 +9,9 @@ public class ShellSort {
 
     public static <T extends Comparable<? super T>> void shellSort(T[] a) {
         int j;
+        //希尔增量排序
         for (int gap = a.length/2; gap>0; gap/=2) {
+            //插入排序
             for (int i = gap; i < a.length; i++) {
                 T tmp = a[i];
                 for (j = i; j >= gap && tmp.compareTo(a[j - gap]) < 0; j -= gap) {
